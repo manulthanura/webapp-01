51e1b0534c5fbd3db4b83e2424878cbefcfc4606
Manul Thanura <manulkarunarathna@gmail.com>
2023-01-22 11:00:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ st.set_page_config(page_title="My App", page_icon=":computer:", layout="wide", i
 st.subheader("Hi, I'm Manul :wave:")
 st.title("Undergraduate Student and Developer")
 st.write("I am an Information and Communication Engineering (ICE) undergraduate student at SLTC Research University. My studies provide me with broad proficiency in computer engineering methods, tools, software packages, and techniques. Alongside this, I am developing hard skills as well as soft skills. As such, I am confident that I can make an instant impact in engineering roles. I love collaborating and making connections, and eager to hear about new updates and opportunities in IT and engineering topics.")
+st.write("[learn more >](https://lk.linkedin.com/in/manulthanura)")
+st.write
 
