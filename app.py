531053e86ce456654ca3d0dfbd5d5489d60779dc
Manul Thanura <manulkarunarathna@gmail.com>
2023-01-22 10:57:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title="My App", page_icon=":smiley:", layout="wide", initial_sidebar_state="expanded")
+# find the path to the icon
+st.set_page_config(page_title="My App", page_icon=":computer:", layout="wide", initial_sidebar_state="expanded")
+
+# ---- Header ----
+st.subheader("Hi, I'm Manul :wave:")
+st.title("Undergraduate Student and Developer")
+st.write("I am an Information and Communication Engineering (ICE) undergraduate student at SLTC Research University. My studies provide me with broad proficiency in computer engineering methods, tools, software packages, and techniques. Alongside this, I am developing hard skills as well as soft skills. As such, I am confident that I can make an instant impact in engineering roles. I love collaborating and making connections, and eager to hear about new updates and opportunities in IT and engineering topics.")
+
